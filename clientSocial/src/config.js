8bad13453234bc821c755e0599c03694800ef93e
akash23-ai <akashbhardwaj2024@gmail.com>
2024-08-15 10:04:23 +0530

    Fixed backend url

--- clientSocial/src/config.js
+++ clientSocial/src/config.js
@@ -1,4 +1,7 @@
-export const BACKEND_URL = "https://social-production-abd9.up.railway.app";
+
+// export const BACKEND_URL = "https://social-production-abd9.up.railway.app";
+
+export const BACKEND_URL = "https://social-idoi.onrender.com"
 
 
 // export const BACKEND_URL  = "http://localhost:3001"
